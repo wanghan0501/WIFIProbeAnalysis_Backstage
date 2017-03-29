2c00185e6c90d78c8516a061df79c56e948161c2
王晗 <caesar.w.h.1996@gmail.com>
2017-03-29 14:52:20 +0800

    Initial commit

---
+++ src/main/java/edu/cs/scu/conf/ConfigurationManager.java
@@ -0,0 +1,117 @@
+package edu.cs.scu.conf;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * 配置加载管理类
+ * <p>
+ * Created by Wang Han on 2017/3/29 14:36.
+ * E-mail address is wanghan0501@vip.qq.com.
+ * Copyright © Wang Han SCU. All Rights Reserved.
+ *
+ * @author Wang Han
+ */
+public class ConfigurationManager {
+    // 配置属性
+    private static Properties properties = new Properties();
+
+    static {
+        InputStream in = ConfigurationManager.class.getClassLoader().getResourceAsStream("my.properties");
+        try {
+            properties.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取关键字对应的配置项
+     *
+     * @param key
+     * @return
+     */
+    private static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+
+    /**
+     * 获取String配置项
+     *
+     * @param key
+     * @return
+     */
+    public static String getString(String key) {
+        return getProperty(key);
+    }
+
+    /**
+     * 获取Integer型配置项
+     *
+     * @param key
+     * @return
+     */
+    public static Integer getInteger(String key) {
+        String value = getProperty(key);
+        try {
+            return Integer.valueOf(value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return 0;
+    }
+
+    /**
+     * 获取Boolean型配置项
+     *
+     * @param key
+     * @return
+     */
+    public static Boolean getBoolean(String key) {
+        String value = getProperty(key);
+        try {
+            return Boolean.valueOf(value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    /**
+     * 获取Long型配置项
+     *
+     * @param key
+     * @return
+     */
+    public static Long getLong(String key) {
+        String value = getProperty(key);
+        try {
+            return Long.valueOf(value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return 0L;
+    }
+
+    /**
+     * 获取Double型配置项
+     *
+     * @param key
+     * @return
+     */
+    public static Double getDouble(String key) {
+        String value = getProperty(key);
+        try {
+            return Double.valueOf(value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return 0.0D;
+    }
+}
---
+++ src/main/java/edu/cs/scu/constants/Constants.java
@@ -0,0 +1,18 @@
+package edu.cs.scu.constants;
+
+/**
+ * 常量接口类
+ * <p>
+ * Created by Wang Han on 2017/3/29 14:40.
+ * E-mail address is wanghan0501@vip.qq.com.
+ * Copyright © Wang Han SCU. All Rights Reserved.
+ *
+ * @author Wang Han
+ */
+public interface Constants {
+    //定义本地运行的线程数量
+    String SPARK_MASTER = "local[4]";
+    String SPARK_LOCAL = "spark.local";
+    String SPARK_APP_NAME_SESSION = "LargeCommercialDataAnalysisTechnologyBasedOnWIFIProbe";
+    String SPARK_STREAMING_COLLECT_TIME = "spark.streaming.collect.time";
+}
