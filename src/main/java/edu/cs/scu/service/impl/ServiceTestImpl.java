a55a3cc472bdec220e394fa0c8e9aa413e2b08fd
mai ciu s <maicius@outlook.com>
2017-03-30 15:04:00 +0800

    Spring mvc 框架搭建完成

--- src/main/java/edu/cs/scu/service/impl/ServiceTestImpl.java
+++ src/main/java/edu/cs/scu/service/impl/ServiceTestImpl.java
@@ -1,7 +1,9 @@
 package edu.cs.scu.service.impl;
 
+import edu.cs.scu.service.ServiceTest;
+
 /**
  * Created by maicius on 2017/3/29.
  */
-public class ServiceTestImpl {
+public class ServiceTestImpl implements ServiceTest {
 }
