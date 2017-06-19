771bb3610cacd70ddf8ac323b6a72e95473ff1ff
王晗 <caesar.w.h.1996@gmail.com>
2017-06-19 17:00:44 +0800

    增加VendorMacBean以及相关数据库查询代码

---
+++ src/main/java/edu/cs/scu/dao/VendorMacDao.java
@@ -0,0 +1,18 @@
+package edu.cs.scu.dao;
+
+import edu.cs.scu.bean.VendorMacBean;
+
+/**
+ * Created by Wang Han on 2017/6/19 16:43.
+ * E-mail address is wanghan0501@vip.qq.com.
+ * Copyright © 2017 Wang Han. SCU. All Rights Reserved.
+ *
+ * @author Wang Han
+ */
+public interface VendorMacDao {
+    // 添加信息
+    void addVendorMac(VendorMacBean vendorMacBean);
+
+    // 根据Mac前缀获取制造商
+    VendorMacBean getVendorByMac(String mac);
+}
---
+++ src/test/java/edu/cs/scu/registermac/Register.java
@@ -0,0 +1,12 @@
+package edu.cs.scu.registermac;
+
+/**
+ * Created by Wang Han on 2017/6/19 16:38.
+ * E-mail address is wanghan0501@vip.qq.com.
+ * Copyright © 2017 Wang Han. SCU. All Rights Reserved.
+ */
+public class Register {
+    public static void main(String args[]){
+
+    }
+}
