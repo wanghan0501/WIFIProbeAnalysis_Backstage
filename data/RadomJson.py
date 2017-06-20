d955df455c24697be5b600544ea1a07592dd07d9
王晗 <caesar.w.h.1996@gmail.com>
2017-06-20 13:52:39 +0800

    格式化

--- data/RadomJson.py
+++ data/RadomJson.py
@@ -1,16 +1,15 @@
-#coding=utf-8
+# coding=utf-8
 import random
-import threading
-from time import ctime,sleep
+from time import ctime, sleep
 import json
-import urllib2
 import time
 import os
+
 
 def random_mac():
     macList = []
     for i in range(1, 7):
-        randStr = "".join(random.sample("0123456789abcdef",2))
+        randStr = "".join(random.sample("0123456789abcdef", 2))
         macList.append(randStr)
     randMac = ":".join(macList)
     return randMac
@@ -24,37 +23,38 @@ def random_range():
     return str(round(random.uniform(0, 100), 1))
 
 
-
 def random_id():
     return str(random.randrange(1, 1000))
 
 
 probeList = []
 
-def random_json(item):
 
-    probe = {"id": ''+random_id(), "mmac": random_mac(), "rate": "3", "wssid": "test", "wmac": random_mac(), "time": time.strftime('%a %b %e %H:%M:%S %Y', time.localtime(time.time()))}
-    mac_data ={"mac": random_mac(), "rssi": random_rssi(), "range": random_range()}
+def random_json(item):
+    probe = {"id": '' + random_id(), "mmac": random_mac(), "rate": "3", "wssid": "test", "wmac": random_mac(),
+             "time": time.strftime('%a %b %e %H:%M:%S %Y', time.localtime(time.time()))}
+    mac_data = {"mac": random_mac(), "rssi": random_rssi(), "range": random_range()}
     mac_DataMul = []
     for i in range(random.randrange(1, 5)):
         mac_DataMul.append(mac_data)
     probe['data'] = mac_DataMul
 
     probe = json.dumps(probe)
-    
-    fileName = '/Users/mac/Workspace/Java/WIFIProbeAnalysis_Backstage/wifiProbe/source/A'+str(item)
+
+    fileName = '/Users/mac/Workspace/Java/WIFIProbeAnalysis_Backstage/wifiProbe/source/A' + str(item)
     file = open(fileName, 'w')
     file.write(probe)
     file.close()
-    os.system('hdfs dfs -put '+fileName+' /source')
-    os.system('rm '+fileName)
+    os.system('hdfs dfs -put ' + fileName + ' /source')
+    os.system('rm ' + fileName)
     sleep(1)
+
 
 if __name__ == '__main__':
     threads = []
     probeList = []
-    index=0
+    index = 0
     for i in range(5):
         random_json(i)
 
-    print "all over %s" %ctime()
+    print "all over %s" % ctime()
